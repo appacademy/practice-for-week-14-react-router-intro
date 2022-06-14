a6a90cec5df92a5abe5192690cf932cebc8bd2cd
Ricky Cheung <96600317+WingNinCheung@users.noreply.github.com>
2022-06-14 13:44:24 -0700

    Done redirect Done redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import Home from "./components/Home";
 import Stocks from "./components/Stocks";
 import Movies from "./components/Movies";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="main">
       <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            <NavLink
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+              to="/movies"
+            >
+              Movie
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+              to="/stocks"
+            >
+              Stock
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path="/">
           <Home />
@@ -16,6 +47,9 @@ function App() {
         </Route>
         <Route path="/movies">
           <Movies />
+        </Route>
+        <Route path="/not-logged-in">
+          <h1> You Must Be Logged In To Enter.</h1>
         </Route>
         <Route path="">
           <h1>Page Not Found</h1>
--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,6 +1,11 @@
+import { Redirect } from "react-router-dom";
+
 function Stocks() {
+  let loggedIn = true;
+
+  if (!loggedIn) return <Redirect to="/" />;
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
     </div>
   );
