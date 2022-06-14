6b8540d59c69106880fe5c9268561a3750d85ff9
Ricky Cheung <96600317+WingNinCheung@users.noreply.github.com>
2022-06-14 13:49:43 -0700

    Finsihed router nav Finsihed router nav

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,12 +1,20 @@
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 
 function Stocks() {
   let loggedIn = true;
+  const history = useHistory();
 
   if (!loggedIn) return <Redirect to="/" />;
+
+  const handleClick = () => {
+    window.alert("sending info to the DB!");
+    console.log(history);
+    history.push("/");
+  };
   return (
     <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
