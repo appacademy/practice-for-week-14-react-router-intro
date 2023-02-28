b5313c294a51cb30494e3ba541da5bbefe74bcf4
Charlie8rown <charlesaharrielson@yahoo.com>
2023-02-28 22:01:35 +0000

    Update 7 files

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,24 @@
+import { useHistory, Redirect } from "react-router-dom";
+
 function Stocks() {
+  const history = useHistory();
+  const loggedIn = true;
+
+  console.log("history: ", history);
+
+  if (!loggedIn) return <Redirect to="/not-logged-in" />;
+
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+
+    history.push("/");
+  };
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
+
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
